e7cdf39582f7feb4b8cafe6cff5ea50df498f50a
karthirajan <karthirajan0706@gmail.com>
2022-04-21 09:37:37 +0800

    New project into git

---
+++ src/org/firstfloor/EigthStandard.java
@@ -0,0 +1,33 @@
+package org.firstfloor;
+
+public class EigthStandard { //pascal or init notations
+	
+	public void jeganKumar(){ // camal notation
+		
+		System.out.println("he is a good learner");
+		
+	}
+	
+	public void ubed(){
+		
+		System.out.println("he is a good cricketer");
+		
+	}
+	
+	public void yogeshWaran(){
+		
+		System.out.println("he is a good dancer");
+		
+	}
+	
+	
+	public static void main(String[] args){
+		
+		EigthStandard e = new EigthStandard();
+		e.ubed();
+		e.yogeshWaran();
+		e.jeganKumar();
+		
+	}
+
+}
