6784b1721d65b5d6b069da4ef2a545d165ce256b
gokulaakannan <gokulakannan4008@gmail>
2022-04-22 03:08:10 -0700

    gokul task

--- src/org/firstfloor/EigthStandard.java
+++ src/org/firstfloor/EigthStandard.java
@@ -17,7 +17,12 @@ public class EigthStandard { //pascal or init notations
 	public void yogeshWaran(){
 		
 		System.out.println("he is a good dancer");
+	}
 		
+	public void kannan() {
+		
+		System.out.println("i done my task");
+	
 	}
 	
 	
@@ -27,7 +32,7 @@ public class EigthStandard { //pascal or init notations
 		e.ubed();
 		e.yogeshWaran();
 		e.jeganKumar();
-		
+		e.kannan();
 	}
 
 }
